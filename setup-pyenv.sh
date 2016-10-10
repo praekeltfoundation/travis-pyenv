11a856048868e484c7a9e8ede6627e8016d46be4
Jamie Hewland <jhewland@gmail.com>
2016-10-10 11:02:06 +0200

    Generalize to a pyenv setup script
    * The PyPy-specific parts are small

--- setup-pyenv.sh
+++ setup-pyenv.sh
@@ -1,8 +1,11 @@
 # NOTE: This script needs to be sourced so it can modify the environment.
 #
 # Environment variables that can be set:
-# - PYPY_VERSION
-#     Version of PyPy2 to install [required]
+# - PYENV_VERSION
+#     Python to install [required]
+# - PYENV_VERSION_PATTERN
+#     Pattern to grep against the output of `python --version` to validate that
+#     the correct Python was installed [default: none]
 # - PYENV_ROOT
 #     Directory in which to install pyenv [default: ~/.pyenv]
 # - PYENV_RELEASE
@@ -10,8 +13,8 @@
 # - PYTHON_BUILD_CACHE_PATH:
 #     Directory in which to cache PyPy builds [default: ~/.pyenv_cache]
 
-if [[ -z "$PYPY_VERSION" ]]; then
-  echo "\$PYPY_VERSION is not set. Not installing PyPy."
+if [[ -z "$PYENV_VERSION" ]]; then
+  echo "\$PYENV_VERSION is not set. Not installing a pyenv."
   return 0
 fi
 
@@ -23,7 +26,7 @@ PYENV_ROOT="${PYENV_ROOT:-$HOME/.pyenv}"
 if [[ -n "$PYENV_RELEASE" ]]; then
   # Fetch the release archive from Github (slightly faster than cloning)
   mkdir "$PYENV_ROOT"
-  curl -SL "https://github.com/yyuu/pyenv/archive/$PYENV_RELEASE.tar.gz" | \
+  curl -fSL "https://github.com/yyuu/pyenv/archive/$PYENV_RELEASE.tar.gz" | \
     tar -xz -C "$PYENV_ROOT" --strip-components 1
 else
   # Don't have a release to fetch, so just clone directly
@@ -37,13 +40,18 @@ eval "$(pyenv init -)"
 PYTHON_BUILD_CACHE_PATH="${PYTHON_BUILD_CACHE_PATH:-$HOME/.pyenv_cache}"
 mkdir -p "$PYTHON_BUILD_CACHE_PATH"
 
-# Install pypy and make a virtualenv for it.
-pyenv install -s pypy-$PYPY_VERSION
-pyenv global pypy-$PYPY_VERSION
-virtualenv -p $(which python) "$HOME/env-pypy-$PYPY_VERSION"
-source "$HOME/env-pypy-$PYPY_VERSION/bin/activate"
+# Install the pyenv (pyenv reads $PYENV_VERSION to know what to install)
+pyenv install
+pyenv global
 
-if ! python --version 2>&1 | fgrep "PyPy $PYPY_VERSION"; then
-  echo "Failed to verify that PyPy was properly installed."
-  return 1
+# Make and source a new virtualenv
+VIRTUAL_ENV="$HOME/ve-pyenv-$PYENV_PYTHON"
+virtualenv -p "$(which python)" "$VIRTUAL_ENV"
+source "$VIRTUAL_ENV/bin/activate"
+
+if [[ -n "$PYENV_VERSION_PATTERN" ]]; then
+  if ! python --version 2>&1 | fgrep -E "$PYENV_VERSION_PATTERN"; then
+    echo "Failed to verify that the pyenv was properly installed."
+    return 1
+  fi
 fi

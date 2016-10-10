458456b34b945605f61c54ac69868524dca8358e
Jamie Hewland <jhewland@gmail.com>
2016-10-10 12:11:14 +0200

    Try add a legacy setup-pypy.sh script for users of the old script

---
+++ setup-pypy.sh
@@ -0,0 +1,26 @@
+# DEPRECATED: Please use setup-pyenv.sh rather.
+# NOTE: This script needs to be sourced so it can modify the environment.
+#
+# Environment variables that can be set:
+# - PYPY_VERSION
+#     Version of PyPy2 to install [required]
+# - PYENV_ROOT
+#     Directory in which to install pyenv [default: ~/.pyenv]
+# - PYENV_RELEASE
+#     Release tag of pyenv to download [default: clone from master]
+# - PYTHON_BUILD_CACHE_PATH:
+#     Directory in which to cache PyPy builds [default: ~/.pyenv_cache]
+
+echo 'WARNING: setup-pypy.sh is *deprecated*. Please use setup-pyenv.sh rather.'
+echo 'setup-pypy.sh will be removed in the next release.'
+
+if [[ -z "$PYPY_VERSION" ]]; then
+  echo "\$PYPY_VERSION is not set. Not installing PyPy."
+  return 0
+fi
+
+export PYENV_VERSION="$PYPY_VERSION"
+export PYENV_VERSION_STRING="PyPy $PYPY_VERSION"
+
+SCRIPT="$(readlink -f $0)"
+source "$(dirname $SCRIPT)"/setup-pyenv.sh
